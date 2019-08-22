0b4c42ec81bc56eb75b1e42345cbab3dd32c7a8f
Bruno Ramalho <bfmramalho@gmail.com>
2019-08-22 23:30:23 +0200

    api ge

---
+++ terraform/api_gateway.tf
@@ -0,0 +1,48 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "it_works_api"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "it_works"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+  api_key_required = false
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.it_works_lambda.invoke_arn}"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = ["aws_api_gateway_integration.integration"]
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "test"
+  description = "${md5(file("${path.module}/api_gateway.tf"))}"
+}
+
+resource "aws_lambda_permission" "api_gw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.it_works_lambda.function_name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_deployment.deployment.execution_arn}/*/*"
+}
+
+output "base_url" {
+  value = "${aws_api_gateway_deployment.deployment.invoke_url}"
+}

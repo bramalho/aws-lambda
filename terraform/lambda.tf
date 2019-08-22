3333840560fee1179842a251a5d1b3a9c7dc0606
Bruno Ramalho <bfmramalho@gmail.com>
2019-08-22 22:26:51 +0200

    add cloudwatch to lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -24,4 +24,39 @@ resource "aws_lambda_function" "it_works_lambda" {
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "it_works.lambda_handler"
   source_code_hash = "${filebase64sha256("${path.module}/../functions/it_works.zip")}"
+  depends_on    = ["aws_iam_role_policy_attachment.lambda_logs", "aws_cloudwatch_log_group.lambda_log_group"]
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/cloudwatch_log_group"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_stream" "lambda_log_stream" {
+  name           = "lambda_log_stream"
+  log_group_name = "${aws_cloudwatch_log_group.lambda_log_group.name}"
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name   = "lambda_logging"
+  path   = "/"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:*"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = "${aws_iam_role.iam_for_lambda.name}"
+  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
 }
